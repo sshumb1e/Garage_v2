3f1bdbc1a4b0e1e20519baaecbaab79950b11e74
s.skvortsov <sergei.skvortsov10@gmail.com>
2023-02-26 21:00:51 +0200

    init commit

---
+++ src/test/java/com/example/garage_v2/GarageV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garage_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
