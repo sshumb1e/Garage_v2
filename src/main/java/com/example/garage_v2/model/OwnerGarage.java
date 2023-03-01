603724e0eaf5a1b8b56ec09d7086bfeb148e1f02
s.skvortsov <sergei.skvortsov10@gmail.com>
2023-03-01 17:50:00 +0200

    add some change

--- src/main/java/com/example/garage_v2/model/OwnerGarage.java
+++ src/main/java/com/example/garage_v2/model/OwnerGarage.java
@@ -1,11 +1,17 @@
 package com.example.garage_v2.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Builder
+@Table("car")
+@AllArgsConstructor
+@NoArgsConstructor
 public class OwnerGarage {
 
     @Column("owner_id")
