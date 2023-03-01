4d72558b15d22ce7506700000d0a148d617a1a55
s.skvortsov <sergei.skvortsov10@gmail.com>
2023-03-01 21:17:54 +0200

    add some change

---
+++ src/main/resources/db/migration/V1_01__insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO "car"(
+    brand, colour)
+VALUES ('Nissan', 'White');
+
+INSERT INTO "car"(
+    brand, colour, owner_id)
+VALUES ('Ford', 'Black', 3);
+
+INSERT INTO "owner"(
+    name, age)
+VALUES ('Tomas', 44), ('Lily', 19);
